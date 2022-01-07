d1dbf35a45ec86e9de9ee8593e8966d43b2a3f36
Franco Biasin <franconbiasin@gmail.com>
2022-01-07 20:05:10 -0300

    Fix auth and cleanup scripts, sleep 60 after auth.sh

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-[[ "$(curl -s "https://www.duckdns.org/update?domains=${DUCKDNS_DOMAIN%.duckdns.org}&token=${DUCKDNS_TOKEN}&txt=${CERTBOT_VALIDATION}")" = "OK" ]]
 
+set -e
+
+DUCKDNS_RESPONSE=$(curl -s "https://www.duckdns.org/update?domains=${DUCKDNS_DOMAIN%.duckdns.org}&token=${DUCKDNS_TOKEN}&txt=${CERTBOT_VALIDATION}")
+echo "Sleeping for 60 seconds"
+
+if [ "$DUCKDNS_RESPONSE" != "OK" ]; then
+  return 1
+fi
--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-[[ "$(curl -s "https://www.duckdns.org/update?domains=${DUCKDNS_DOMAIN%.duckdns.org}&token=${DUCKDNS_TOKEN}&txt=${CERTBOT_VALIDATION}&clear=true")" = "OK" ]]
+
+set -e
+
+DUCKDNS_RESPONSE=$(curl -s "https://www.duckdns.org/update?domains=${DUCKDNS_DOMAIN%.duckdns.org}&token=${DUCKDNS_TOKEN}&txt=${CERTBOT_VALIDATION}&clear=true")
+
+if [ "$DUCKDNS_RESPONSE" != "OK" ]; then
+  return 1
+fi
